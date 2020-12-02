69d8bbc14ac837e626ed46752a08d0450fff26a0
Ygor Santos <ygor.maniz@gmail.com>
2020-12-02 00:09:11 -0300

    Add first test to getAllPairs function

--- src/test/kotlin/util/ListTest.kt
+++ src/test/kotlin/util/ListTest.kt
@@ -1,13 +1,16 @@
 package util
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class ListTest {
     @Test
     fun `Should return valid pair for two items`() {
         val input = listOf(1, 5)
-        
-        input.getAllPairs { i1, i2 ->  }
+
+        val output: MutableList<Pair<Int, Int>> = mutableListOf()
+        input.getAllPairs { i1, i2 -> output.add(Pair(i1, i2)) }
+
+        assertEquals(listOf(Pair(1, 5)), output)
     }
 }
